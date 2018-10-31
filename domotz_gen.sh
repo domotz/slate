7f8056e2958b6465c2caad6a5c490d7177f6eff1
domotz_azza <a.azzara@domotz.com>
2018-10-31 19:24:16 +0100

    Added domotz_templates folder

--- domotz_gen.sh
+++ domotz_gen.sh
@@ -2,4 +2,4 @@
 
 curl https://api-eu-west-1-cell-1.domotz.com/public-api/v1/meta/open-api-definition > domotz_open_api.json
 
-docker run -w /local -v ${PWD}:/local -it registry.domotz.com/domotz/domotz_slate:1.0.0 bash -c "widdershins domotz_open_api.json -o source/index.html.md && bundle exec middleman build"
+docker run -w /local -v ${PWD}:/local -it registry.domotz.com/domotz/domotz_slate:1.0.0 bash -c "widdershins -u domotz_templates domotz_open_api.json -o source/index.html.md && bundle exec middleman build"
