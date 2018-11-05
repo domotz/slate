684253aa622c695730239033ac2233c3b9c7d16e
domotz_azza <a.azzara@domotz.com>
2018-11-05 14:41:09 +0100

    replaced api base endpoint

--- domotz_gen.js
+++ domotz_gen.js
@@ -2,7 +2,7 @@ const converter = require('widdershins');
 const fs = require('fs');
 const fetch = require('node-fetch');
 const apiDefBaseUrl = 'api-eu-west-1-cell-1.domotz.com';
-const apiDefDocUrl = 'api-base.domotz.com';
+const apiDefDocUrl = 'your-domotz-api-endpoint';
 
 let options = {};
 options.user_templates = './domotz_templates';
@@ -12,7 +12,9 @@ options.templateCallback = function (templateName, stage, data) {
 options.theme = 'darkula';
 options.search = true;
 options.codeSamples = true;
-options.templateCallback = function(templateName,stage,data) { return data };
+options.templateCallback = function (templateName, stage, data) {
+  return data
+};
 options.sample = true; // set false by --raw
 options.language_tabs = [{ "shell": "Curl" },{ "javascript": "JavaScript" }, { "javascript--nodejs": "Node.JS" }, { "python": "Python" }, { "ruby": "Ruby" }, { "go": "Go" }];
 
@@ -20,7 +22,7 @@ options.language_tabs = [{ "shell": "Curl" },{ "javascript": "JavaScript" }, { "
 fetch(`https://${apiDefBaseUrl}/public-api/v1/meta/open-api-definition`, {method: 'GET',})
   .then(function (res) {
     res.json().then(function (s) {
-      let data = JSON.parse(JSON.stringify(s).replace(apiDefBaseUrl, apiDefDocUrl));
+      let data = JSON.parse(JSON.stringify(s).replace('https://' + apiDefBaseUrl, apiDefDocUrl));
       converter.convert(data, options, function (err, str) {
         fs.writeFileSync("source/index.html.md", str);
       });
