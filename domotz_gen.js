699434a2d3d3026705b1a90037bcdde2250597db
domotz_azza <a.azzara@domotz.com>
2019-03-14 16:27:03 +0100

    use predictable stringify lib to get a consistently sorted document

--- domotz_gen.js
+++ domotz_gen.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const fetch = require('node-fetch');
 const apiDefBaseUrl = 'api-staging-eu-central-1-cell-1.domotz.co';
 const apiDefDocUrl = 'your-domotz-api-endpoint';
+var stringify = require('json-stable-stringify');
+
 
 let options = {};
 options.user_templates = './domotz_templates';
@@ -18,11 +20,10 @@ options.templateCallback = function (templateName, stage, data) {
 options.sample = true; // set false by --raw
 options.language_tabs = [{ "shell": "Curl" },{ "javascript": "JavaScript" }, { "javascript--nodejs": "Node.JS" }, { "python": "Python" }, { "ruby": "Ruby" }, { "go": "Go" }];
 
-
-fetch(`https://${apiDefBaseUrl}/public-api/v1/meta/open-api-definition`, {method: 'GET',})
+fetch(`http://${apiDefBaseUrl}/public-api/v1/meta/open-api-definition`, {method: 'GET',})
   .then(function (res) {
     res.json().then(function (s) {
-      let data = JSON.parse(JSON.stringify(s).replace('https://' + apiDefBaseUrl, apiDefDocUrl));
+      let data = JSON.parse(stringify(s).replace('https://' + apiDefBaseUrl, apiDefDocUrl));
       converter.convert(data, options, function (err, str) {
         fs.writeFileSync("source/index.html.md", str);
       });
