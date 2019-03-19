de4a9e8724c13b62a0d813ba80eaba9fb99bc375
domotz_azza <a.azzara@domotz.com>
2019-03-19 14:37:32 +0100

    US-1547: new base image allowing for open api validation. New custom template for api callbacks

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ WORKDIR /local
 
 RUN bundle install --system
 
-RUN npm install -g widdershins@3.6.6 node-fetch@2.2.0 json-stable-stringify@1.0.1
+RUN npm install -g widdershins@3.6.6 node-fetch@2.2.0 json-stable-stringify@1.0.1 swagger-cli@2.2.1
--- domotz_gen.sh
+++ domotz_gen.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker run -w /local -v ${PWD}:/local -it registry.domotz.com/domotz/domotz_slate:1.0.1 \
+docker run -w /local -v ${PWD}:/local -it registry.domotz.com/domotz/domotz_slate:1.0.2 \
              bash -c 'export NODE_PATH="$(npm config get prefix)/lib/node_modules" && node domotz_gen.js && bundle exec middleman build'
