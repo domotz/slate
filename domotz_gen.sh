808570f6a3c07ff7a4d9c653f700546cbaaf7615
domotz_azza <a.azzara@domotz.com>
2023-09-06 11:30:08 +0200

    removed unnecessary it to allow building on CI

--- domotz_gen.sh
+++ domotz_gen.sh
@@ -2,4 +2,3 @@
 
 docker run -w /local -v ${PWD}:/local registry.domotz.com/domotz/domotz_slate:1.0.3 \
              bash -c 'export NODE_PATH="$(npm config get prefix)/lib/node_modules" && node domotz_gen.js && bundle exec middleman build'
-nn
